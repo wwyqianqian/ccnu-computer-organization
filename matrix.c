c95dd2bf1ea1bfd62062e3914717cc71a0834d8d
wwyqianqian <1208331933@qq.com>
2019-04-10 18:43:21 +0800

    [feat]: kij ikj ijk jik jki kji 添加变量r

--- matrix.c
+++ matrix.c
@@ -34,8 +34,9 @@ int main() {
     ////***算法开始***////
     for (int k = 0; k < n; k++) {
         for (int i = 0; i < m; i++) {
+        	int r = A[i][k];
             for (int j = 0; j < p; j++) {
-                C[i][j] += A[i][k] * B[k][j];
+                C[i][j] += r * B[k][j];
             }
         }
     }
@@ -46,8 +47,9 @@ int main() {
     ////***算法开始***////
     for (int i = 0; i < m; i++) {
         for (int k = 0; k < n; k++) {
+        	int r = A[i][k];
             for (int j = 0; j < p; j++) {
-                C[i][j] += A[i][k] * B[k][j];
+                C[i][j] += r * B[k][j];
             }
         }
     }
@@ -60,9 +62,11 @@ int main() {
     ////***算法开始***////
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < p; j++) {
+        	int sum = 0;
             for (int k = 0; k < n; k++) {
-                C[i][j] += A[i][k] * B[k][j];
+                sum += A[i][k] * B[k][j];
             }
+            C[i][j] += sum;
         }
     }
     ////***算法结束***////
@@ -72,9 +76,11 @@ int main() {
     ////***算法开始***////
     for (int j = 0; j < p; j++) {
         for (int i = 0; i < m; i++) {
+        	int sum = 0;
             for (int k = 0; k < n; k++) {
-                C[i][j] += A[i][k] * B[k][j];
+                sum += A[i][k] * B[k][j];
             }
+            C[i][j] += sum;
         }
     }
     ////***算法结束***////
@@ -86,8 +92,9 @@ int main() {
     ////***算法开始***////
     for (int j = 0; j < p; j++) {
         for (int k = 0; k < n; k++) {
+        	int r = B[k][j];
             for (int i = 0; i < m; i++) {
-                C[i][j] += A[i][k] * B[k][j];
+                C[i][j] += A[i][k] * r;
             }
         }
     }
@@ -98,8 +105,9 @@ int main() {
     ////***算法开始***////
     for (int k = 0; k < n; k++) {
         for (int j = 0; j < p; j++) {
+        	int r = B[k][j];
             for (int i = 0; i < m; i++) {
-                C[i][j] += A[i][k] * B[k][j];
+                C[i][j] += A[i][k] * r;
             }
         }
     }
@@ -120,3 +128,11 @@ int main() {
 // jik time: 12.905352 sec
 // jki time: 37.395260 sec
 // kji time: 34.362129 sec
+
+//  ~  ./mx     
+// kij time: 8.908832 sec
+// ikj time: 9.080675 sec
+// ijk time: 13.892830 sec
+// jik time: 12.771233 sec
+// jki time: 39.090536 sec
+// kji time: 35.421174 sec
