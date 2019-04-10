5dc2e2a061ad97d9dd9f20855d098e6f3358d6cb
wwyqianqian <1208331933@qq.com>
2019-04-10 18:20:58 +0800

    [feat]: kij ikj ijk jik jki kji

--- matrix.c
+++ matrix.c
@@ -6,7 +6,6 @@
 #define n 1000
 #define p 2000
 
-//创建矩阵数组
 int A[m][n];
 int B[n][p];
 int C[m][p];
@@ -33,20 +32,6 @@ int main() {
 
     gettimeofday(&st, NULL);
     ////***算法开始***////
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < p; j++) {
-            for (int k = 0; k < n; k++) {
-                C[i][j] += A[i][k] * B[k][j];
-            }
-        }
-    }
-    ////***算法结束***////
-    gettimeofday(&et, NULL);
-    printf("ijk time: %0.6lf sec\n", et.tv_sec + et.tv_usec * 1e-6 - st.tv_sec - st.tv_usec * 1e-6);
-
-
-    gettimeofday(&st, NULL);
-    ////***算法开始***////
     for (int k = 0; k < n; k++) {
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < p; j++) {
@@ -57,22 +42,6 @@ int main() {
     ////***算法结束***////
     gettimeofday(&et, NULL);
     printf("kij time: %0.6lf sec\n", et.tv_sec + et.tv_usec * 1e-6 - st.tv_sec - st.tv_usec * 1e-6);
-
-
-    gettimeofday(&st, NULL);
-    ////***算法开始***////
-    for (int k = 0; k < n; k++) {
-        for (int j = 0; j < p; j++) {
-            for (int i = 0; i < m; i++) {
-                C[i][j] += A[i][k] * B[k][j];
-            }
-        }
-    }
-    ////***算法结束***////
-    gettimeofday(&et, NULL);
-    printf("kji time: %0.6lf sec\n", et.tv_sec + et.tv_usec * 1e-6 - st.tv_sec - st.tv_usec * 1e-6);
-
-
     gettimeofday(&st, NULL);
     ////***算法开始***////
     for (int i = 0; i < m; i++) {
@@ -86,39 +55,68 @@ int main() {
     gettimeofday(&et, NULL);
     printf("ikj time: %0.6lf sec\n", et.tv_sec + et.tv_usec * 1e-6 - st.tv_sec - st.tv_usec * 1e-6);
 
-    
-    //输出结果
-/*    for(int i=0; i<m; i++) {
-        for (int j = 0; j < n; j++) {
-            printf("%.0f",A[i][j]);
-            printf(" ");
-        }
-        printf("\n");
-    }
-    printf("*\n");
-    for(int i=0; i<n; i++) {
-        for(int j=0; j<p; j++) {
-            printf("%.0f",B[i][j]);
-            printf(" ");
-        }
-        printf("\n");
-    }
-    printf("=\n");
-    for(int i=0; i<m; i++) {
+
+    gettimeofday(&st, NULL);
+    ////***算法开始***////
+    for (int i = 0; i < m; i++) {
         for (int j = 0; j < p; j++) {
-            printf("%.0f",C[i][j]);
-            printf(" ");
+            for (int k = 0; k < n; k++) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
         }
-        printf("\n");
-    }*/
+    }
+    ////***算法结束***////
+    gettimeofday(&et, NULL);
+    printf("ijk time: %0.6lf sec\n", et.tv_sec + et.tv_usec * 1e-6 - st.tv_sec - st.tv_usec * 1e-6);
+    gettimeofday(&st, NULL);
+    ////***算法开始***////
+    for (int j = 0; j < p; j++) {
+        for (int i = 0; i < m; i++) {
+            for (int k = 0; k < n; k++) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+    ////***算法结束***////
+    gettimeofday(&et, NULL);
+    printf("jik time: %0.6lf sec\n", et.tv_sec + et.tv_usec * 1e-6 - st.tv_sec - st.tv_usec * 1e-6);
+
+
+    gettimeofday(&st, NULL);
+    ////***算法开始***////
+    for (int j = 0; j < p; j++) {
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < m; i++) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+    ////***算法结束***////
+    gettimeofday(&et, NULL);
+    printf("jki time: %0.6lf sec\n", et.tv_sec + et.tv_usec * 1e-6 - st.tv_sec - st.tv_usec * 1e-6);
+    gettimeofday(&st, NULL);
+    ////***算法开始***////
+    for (int k = 0; k < n; k++) {
+        for (int j = 0; j < p; j++) {
+            for (int i = 0; i < m; i++) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+    ////***算法结束***////
+    gettimeofday(&et, NULL);
+    printf("kji time: %0.6lf sec\n", et.tv_sec + et.tv_usec * 1e-6 - st.tv_sec - st.tv_usec * 1e-6);
+   
     return 0;
 }
 
 
 
-//  ~/Desktop  gcc mx.c -o mx
-//  ~/Desktop  ./mx
-// ijk time: 15.744352 sec
-// kij time: 9.360390 sec
-// kji time: 39.164687 sec
-// ikj time: 9.909856 sec
+//  ~  gcc /Users/qianqian/Desktop/mx.c -o mx             Wed Apr 10 18:16:12 2019
+//  ~   ./mx                                      807ms  Wed Apr 10 18:16:18 2019
+// kij time: 9.116704 sec
+// ikj time: 9.091694 sec
+// ijk time: 15.381990 sec
+// jik time: 12.905352 sec
+// jki time: 37.395260 sec
+// kji time: 34.362129 sec
